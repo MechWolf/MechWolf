233ae0e34510ec0d10358d77aa7cb26ff4697f55
amijalis <mijalis@gmail.com>
2018-01-15 23:23:09 -0500

    Implemented a threaded MQTT message parser for the pycom WiPi 2.0, connecting to amazon AWS IOT service.
     Changes to be committed:
    	modified:   boot.py
    	new file:   cert/.DS_Store
    	new file:   cert/certificate.crt
    	new file:   cert/privateKey.key
    	new file:   cert/publicKey.key
    	new file:   cert/root-ca.pem
    	new file:   lib/.DS_Store
    	new file:   lib/awsconfig.py
    	deleted:    lib/awsiot_sign.py
    	deleted:    lib/hmac_ltd.py
    	deleted:    lib/simple.py
    	deleted:    lib/urequests.py
    	deleted:    lib/urequests_old.py
    	modified:   main.py

--- micropython/boot.py
+++ micropython/boot.py
@@ -1,18 +1,27 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
+from network import WLAN
+import os
+from machine import UART, RTC
+import pycom
+import time
+#import ntptime
 
-import network
-import ntptime
+
 
 def do_connect():
 
-    sta_if = network.WLAN(network.STA_IF)
+    sta_if = WLAN(mode=WLAN.STA)
     if not sta_if.isconnected():
         print('connecting to network...')
-        sta_if.active(True)
-        sta_if.connect('maple-ave-cast','susannasung')
+        sta_if.connect('maple-ave-cast', auth=(None,'susannasung'))
         while not sta_if.isconnected():
-            pass
+            time.sleep(0.5)
     print('network config:', sta_if.ifconfig())
 
+
+uart = UART(0, 115200)
+os.dupterm(uart)
+pycom.heartbeat(False)
+rtc = RTC()
 do_connect()
-ntptime.settime()
+rtc.ntp_sync('pool.ntp.org')
--- micropython/main.py
+++ micropython/main.py
@@ -1,26 +1,35 @@
-import json
+from MQTTLib import AWSIoTMQTTClient
 import time
-import urequests as requests
-import awsiot_sign
-import machine
+import awsconfig
+import json
 
-def timestamp():
-	return "%04u%02u%02uT%02u%02u%02uZ" % time.localtime()[0:6]
 
-def toggle(led):
-	led.value(not led.value())
+# user specified callback function
+def customCallback(client, userdata, message):
+	print("Received a new message: ")
+	message = json.loads(message.payload)
+	for key,value in message.items():
+		print(key,value)
 
-file = open('aws_info.txt')
-keys = file.readline()
-keys = json.loads(keys)
-led = machine.Pin(0, machine.Pin.OUT)
+time.sleep(5)
+# awsconfigure the MQTT client
+pycomAwsMQTTClient = AWSIoTMQTTClient(awsconfig.CLIENT_ID)
+pycomAwsMQTTClient.configureEndpoint(awsconfig.AWS_HOST, awsconfig.AWS_PORT)
+pycomAwsMQTTClient.configureCredentials(awsconfig.AWS_ROOT_CA, awsconfig.AWS_PRIVATE_KEY, awsconfig.AWS_CLIENT_CERT)
 
-while True:
-	request_dict = awsiot_sign.request_gen(keys['endpt_prefix'], 'pump0', keys['akey'], keys['skey'], timestamp(), region=keys['region'], method='GET')
-	endpoint = 'https://' + request_dict["host"] + request_dict["uri"]
-	r = requests.get(endpoint, headers=request_dict["headers"])
-	if r.status_code == 200:
-		req = json.loads(r.text)
-		if req['state']['desired']['light'] == led.value():
-			toggle(led)
-	time.sleep(1)
+pycomAwsMQTTClient.configureOfflinePublishQueueing(awsconfig.OFFLINE_QUEUE_SIZE)
+pycomAwsMQTTClient.configureDrainingFrequency(awsconfig.DRAINING_FREQ)
+pycomAwsMQTTClient.configureConnectDisconnectTimeout(awsconfig.CONN_DISCONN_TIMEOUT)
+pycomAwsMQTTClient.configureMQTTOperationTimeout(awsconfig.MQTT_OPER_TIMEOUT)
+pycomAwsMQTTClient.configureLastWill(awsconfig.LAST_WILL_TOPIC, awsconfig.LAST_WILL_MSG, 1)
+
+#Connect to MQTT Host
+if pycomAwsMQTTClient.connect():
+    print('AWS connection succeeded')
+else:
+	print("Oops...")
+# Subscribe to topic
+pycomAwsMQTTClient.subscribe(awsconfig.TOPIC, 1, customCallback)
+time.sleep(2)
+
+
