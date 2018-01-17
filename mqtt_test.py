2844ab0268c08f749e819fe4f76c092da52571f4
amijalis <mijalis@gmail.com>
2018-01-16 20:11:19 -0500

    Added the required certificate
     Changes to be committed:
    	new file:   cert/aws_root_auth.pem
    	modified:   mqtt_test.py

--- mqtt_test.py
+++ mqtt_test.py
@@ -1,13 +1,13 @@
 #A test script for connecting to our mqtt server and dealing with messages.
 #Before running, set your environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY to their corresponding values from the keys file. os.environ["AWS_ACCESS_KEY_ID"] should return this value.
-
+#Also, make sure you have installed the root-ca in the cert/ folder.
 from AWSIoTPythonSDK import MQTTLib
 
 def onMessage(client,userdata,message):
     print(message.topic,message.payload)
 
 client = MQTTLib.AWSIoTMQTTClient('server',useWebsocket=True)
-client.configureCredentials('aws_root_auth.pem')
+client.configureCredentials('cert/aws_root_auth.pem')
 client.configureEndpoint('a365awttlmyft7.iot.us-east-1.amazonaws.com',443)
 client.connect()
 client.subscribe('pump0',0,onMessage)
