0b5bccf2af7c424523da4974ce56a8c8566cedf2
amijalis <mijalis@gmail.com>
2018-01-18 17:48:24 -0500

    Updating naming scheme
     Changes to be committed:
    	modified:   connection.py

--- connection.py
+++ connection.py
@@ -20,37 +20,39 @@ class Connection:
     def __init__(self, certificate = 'cert/aws_root_auth.pem', 
                        endpoint_url = 'a365awttlmyft7.iot.us-east-1.amazonaws.com'): 
         
-        self.objects = {}
+        self.devices = {}
         self.client = MQTTLib.AWSIoTMQTTClient('server',useWebsocket=True)
         self.client.configureCredentials('cert/aws_root_auth.pem')
         self.client.configureEndpoint(endpoint_url,443)
         self.client.connect()
-
-    def _add_object(self, object_id, quality_of_service = 0):
         
-        if self.client.subscribe(object_id, quality_of_service, self.on_message):
-            self.objects[object_id] = {'connected': False}
-            if not self.client.publish(object_id, '{"ping":1}', 1):
-                raise RuntimeError('Unable to send message')
+        if not self.client.subscribe('server', quality_of_service, self.on_message):
+            raise Warning("Unable to subscribe to server topic")
+
+    def _add_object(self, device_id, quality_of_service = 0):
+        
+        self.devices[device_id] = {'connected': False}
+        if not self.client.publish(device_id, '{"ping":1}', 1):
+            raise RuntimeError('Unable to send message')
         else:
             raise RuntimeError('Unable to subscribe to topic')
 
     def on_message(self, client, userdata, message):
-        object_id = message.topic
         try:
             message_content = json.loads(message.payload)
+            device_id = message_content['device_id']
         except:
             raise Warning('Received message contained invalid json')
             return
 
         if 'connected' in message_content:
-            if object_id in self.objects:
-                self.objects[object_id]['connected'] = True
+            if device_id in self.devices:
+                self.devices[device_id]['connected'] = True
         elif 'some other keyword' in message_content:
             pass
 
-    def connected(self, object_id):
-        if object_id in self.objects:
-            return self.objects[object_id]['connected']
+    def connected(self, device_id):
+        if device_id in self.devices:
+            return self.devices[device_id]['connected']
         else:
             return False
