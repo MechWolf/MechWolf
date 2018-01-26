476074b49a4eff0645ba6bd3f736ca293b66e315
amijalis <mijalis@gmail.com>
2018-01-26 16:21:43 -0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
    Added a Future/Executor interface for managing tasks submitted to
    devices via the AWS IoT cloud.
    
     Changes to be committed:
    	new file:   AWSIoTPythonSDK/MQTTLib.py
    	new file:   AWSIoTPythonSDK/__init__.py
    	new file:   AWSIoTPythonSDK/core/.DS_Store
    	new file:   AWSIoTPythonSDK/core/__init__.py
    	new file:   AWSIoTPythonSDK/core/greengrass/__init__.py
    	new file:   AWSIoTPythonSDK/core/greengrass/discovery/__init__.py
    	new file:   AWSIoTPythonSDK/core/greengrass/discovery/models.py
    	new file:   AWSIoTPythonSDK/core/greengrass/discovery/providers.py
    	new file:   AWSIoTPythonSDK/core/protocol/.DS_Store
    	new file:   AWSIoTPythonSDK/core/protocol/__init__.py
    	new file:   AWSIoTPythonSDK/core/protocol/connection/__init__.py
    	new file:   AWSIoTPythonSDK/core/protocol/connection/cores.py
    	new file:   AWSIoTPythonSDK/core/protocol/internal/__init__.py
    	new file:   AWSIoTPythonSDK/core/protocol/internal/clients.py
    	new file:   AWSIoTPythonSDK/core/protocol/internal/defaults.py
    	new file:   AWSIoTPythonSDK/core/protocol/internal/events.py
    	new file:   AWSIoTPythonSDK/core/protocol/internal/queues.py
    	new file:   AWSIoTPythonSDK/core/protocol/internal/requests.py
    	new file:   AWSIoTPythonSDK/core/protocol/internal/workers.py
    	new file:   AWSIoTPythonSDK/core/protocol/mqtt_core.py
    	new file:   AWSIoTPythonSDK/core/protocol/paho/__init__.py
    	new file:   AWSIoTPythonSDK/core/protocol/paho/client.py
    	new file:   AWSIoTPythonSDK/core/shadow/__init__.py
    	new file:   AWSIoTPythonSDK/core/shadow/deviceShadow.py
    	new file:   AWSIoTPythonSDK/core/shadow/shadowManager.py
    	new file:   AWSIoTPythonSDK/core/util/__init__.py
    	new file:   AWSIoTPythonSDK/core/util/enums.py
    	new file:   AWSIoTPythonSDK/core/util/providers.py
    	new file:   AWSIoTPythonSDK/exception/AWSIoTExceptions.py
    	new file:   AWSIoTPythonSDK/exception/__init__.py
    	new file:   AWSIoTPythonSDK/exception/operationError.py
    	new file:   AWSIoTPythonSDK/exception/operationTimeoutException.py
    	modified:   connection.py

--- connection.py
+++ connection.py
@@ -1,58 +1,116 @@
 # connection.py 
 # Manages connection to AWS IoT
-# Todo: Use Asyncio so that message waiting does not block.
-# Todo: define schema and use to validate json
-# Todo: add unit test
-# from connection import Connection
-# c = Connection()
-# c.add_object('test')
-# c.connected('test')
-# Should return false
-# c.client.publish('test','{"connected":1}',1)
-# c.connected('test')
-# should return true
 
+import uuid
+from concurrent.futures import _base
 from AWSIoTPythonSDK import MQTTLib
+from collections import deque
 import json
 
-class Connection:
+class _DeviceWorkItem(object):
+    def __init__(self, *, future, device_id, task_id, kwargs):
+        self.future = future
+        self.device_id = device_id
+        self.task_id = task_id
+        self.kwargs = kwargs
 
+    def run(self):
+        if not self.future.set_running_or_notify_cancel():
+            return
+
+    #TODO Fill in the rest here
+
+class DeviceExecutor(_base.Executor):
+
+    def __init__(self, connection=None):
+        self.connection=connection
+        self._task_queue = deque()
+
+    def submit(self, device_id, *args, **kwargs):
+        task_id = uuid.uuid1().hex
+        f = _base.Future()
+        w = _DeviceWorkItem(future = f, device_id = device_id, task_id = task_id, kwargs = kwargs)
+        self._task_queue.append(w)
+        self.send_to_devices()
+        return f
+
+    def send_to_devices(self):
+        for task in self._task_queue:
+            if task.future._state == 'PENDING':
+                self.connection.send(device_id = task.device_id, task_id = task.task_id, message = task.kwargs, callback = self.state_callback)
+
+    def state_callback(self, task_id, state):
+        #Optimize?
+        for task in self._task_queue:
+            if task.task_id == task_id:
+                task.future._state = state
+        
+
+class Connection:
+    # Manages the connection to the AWS message broker.
     def __init__(self, certificate = 'cert/aws_root_auth.pem', 
                        endpoint_url = 'a365awttlmyft7.iot.us-east-1.amazonaws.com'): 
         
-        self.devices = {}
+        self.components = {}
         self.client = MQTTLib.AWSIoTMQTTClient('server',useWebsocket=True)
         self.client.configureCredentials('cert/aws_root_auth.pem')
         self.client.configureEndpoint(endpoint_url,443)
-        self.client.connect()
+        self.tasks = {}
         
-        if not self.client.subscribe('server', quality_of_service, self.on_message):
+    def connect(self):
+        self.client.connect()  
+        if not self.client.subscribe('server', 1, self.on_message):
             raise Warning("Unable to subscribe to server topic")
 
-    def _add_object(self, device_id, quality_of_service = 0):
-        
-        self.devices[device_id] = {'connected': False}
-        if not self.client.publish(device_id, '{"ping":1}', 1):
-            raise RuntimeError('Unable to send message')
+    def register(self, *, task_id, callback):
+        if task_id in self.tasks:
+            return False
         else:
-            raise RuntimeError('Unable to subscribe to topic')
+            self.tasks[task_id] = callback
+            return True
 
+    def send(self, device_id, task_id, message, callback):
+        
+        self.register(task_id = task_id, callback = callback)
+
+        message['task_id'] = task_id
+        message['device_id'] = device_id
+
+        if not self.client.publish(device_id, json.dumps(message), 1):
+            raise RuntimeError('unable to send message')
+
+        return True
+
+    
     def on_message(self, client, userdata, message):
         try:
             message_content = json.loads(message.payload)
             device_id = message_content['device_id']
         except:
-            raise Warning('Received message contained invalid json')
+            raise Warning('Received message contained invalid json, or did not contain device_id key.')
             return
+        
+        if 'task_id' and 'state' in message_content:
+            task_id = message_content['task_id']
+            state = message_content['state']
+            print('received task update!')
+            callback = self.tasks[task_id]
+            callback(task_id, state)
 
         if 'connected' in message_content:
-            if device_id in self.devices:
-                self.devices[device_id]['connected'] = True
+            if component_name in self.components:
+                self.components[component_name].connected = True
         elif 'some other keyword' in message_content:
             pass
 
-    def connected(self, device_id):
-        if device_id in self.devices:
-            return self.devices[device_id]['connected']
+    def is_connected(self, component):
+        if component.name in self.components:
+            return self.components[component.name].connected
         else:
             return False
+
+    def _connect_component(self, component, quality_of_service = 0):
+        self.components[component.name] = component
+        if not self.client.publish(component.name, '{"ping":1}', 1):
+            raise RuntimeError('Unable to send message')
+
