535a09ada8b35a58c91dc3606dd9a7f95e0968f8
Benjamin Lee <benjamindlee@me.com>
2018-02-24 20:01:12 -0500

    don't crash if no protocol given but client requests one

--- app.py
+++ app.py
@@ -25,21 +25,25 @@ def submit_protcol():
 def protocol():
     '''endpoint for devices to check for new protocols to be posted'''
 
-    # once every device has gotten the protocol, give them the start time
-    if all([x in db.Set("acks") for x in list(json.loads(db["protocol"]))]):
-        try:
-            return db["start_time"]
-        except KeyError:
-            db["start_time"] = time() + 15
-            return db["start_time"]
+    try:
+        # if every device has gotten the protocol, give them the start time
+        if all([x in db.Set("acks") for x in list(json.loads(db["protocol"]))]):
+            try:
+                return db["start_time"]
+            except KeyError:
+                db["start_time"] = time() + 15
+                return db["start_time"]
 
-    # store the device that checked in and got the protocol
-    db.Set("acks").add(request.form["device_id"])
+        # otherwise, store the device that checked in and and return the protocol
+        db.Set("acks").add(request.form["device_id"])
+        return app.response_class(
+            response=json.dumps(json.loads(db["protocol"])[request.form.get("device_id")]),
+            status=200,
+            mimetype="application/json")
 
-    return app.response_class(
-        response=json.dumps(json.loads(db["protocol"])[request.form.get("device_id")]),
-        status=200,
-        mimetype="application/json")
+    # if no protocol has been given, return Null
+    except KeyError:
+        return jsonify(None)
 
 
 if __name__ == "__main__":
