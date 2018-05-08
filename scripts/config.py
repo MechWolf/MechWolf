49d0c8df01651bf4a04f027d164f77eff1acc765
amijalis <mijalis@gmail.com>
2018-05-08 14:31:22 -0400

    Include all links to serial ports for connected devices when using scripts/config.py.
     Changes to be committed:
    	modified:   scripts/config.py

--- scripts/config.py
+++ scripts/config.py
@@ -124,7 +124,7 @@ if device_type == "client":
     config = {}
     for i in device_obj(name="setup").config().items():
         if i[0] == 'serial_port':
-            config[i[0]]  = pick([port[0] for port in list_ports.comports()], "Select the serial port your device is connected to:", indicator = '->')[0]
+            config[i[0]]  = pick([port[0] for port in list_ports.comports(include_links=True)], "Select the serial port your device is connected to:", indicator = '->')[0]
         else:
             config[i[0]] = click.prompt(i[0], type=i[1][0], default=i[1][1])
     config_data["device_info"]["device_settings"] = config
