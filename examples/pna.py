6eb79be52bc5869968b1e5eb5fa3d0c0a248ae65
Benjamin Lee <benjamindlee@me.com>
2018-06-24 23:48:30 -0600

    PNA PEP8 Fixes

--- examples/pna.py
+++ examples/pna.py
@@ -5,8 +5,8 @@ from datetime import timedelta
 hatu = mw.Vessel("HATU", name="hatu")
 dmf = mw.Vessel("DMF", name="dmf")
 output = mw.Vessel("waste", name="output")
-diea = mw.Vessel("DIEA", name = "diea")
-pip = mw.Vessel("40% Piperidine in DMF", name = "pip")
+diea = mw.Vessel("DIEA", name="diea")
+pip = mw.Vessel("40% Piperidine in DMF", name="pip")
 
 # define pumps
 coupling_pump = mw.VarianPump(name="coupling_pump")
@@ -26,20 +26,20 @@ fmoc_lys_oh = mw.Vessel("fmoc_lys_oh", name="fmoc_lys_oh")
 # heater = mw.Component("heater")
 
 # define valve
-amino_mapping = dict(fmoc_pna_a = 1,
-                     fmoc_pna_t = 2,
-                     fmoc_pna_c = 3,
-                     fmoc_pna_g = 4,
-                     fmoc_lys_oh = 5,
-                     dmf = 10)
+amino_mapping = dict(fmoc_pna_a=1,
+                     fmoc_pna_t=2,
+                     fmoc_pna_c=3,
+                     fmoc_pna_g=4,
+                     fmoc_lys_oh=5,
+                     dmf=10)
 
-amino_valve = mw.ViciValve(name = "amino_valve", mapping = amino_mapping)
+amino_valve = mw.ViciValve(name="amino_valve", mapping=amino_mapping)
 
-act_mapping = dict(hatu = 1,
-                   pip = 9,
-                   dmf  = 10)
+act_mapping = dict(hatu=1,
+                   pip=9,
+                   dmf=10)
 
-coupling_valve = mw.ViciValve(name = "coupling_valve", mapping = act_mapping)
+coupling_valve = mw.ViciValve(name="coupling_valve", mapping=act_mapping)
 
 fat_tube = mw.Tube(length="2 foot", ID="1/16 in", OD="1/8 in", material="PFA")
 thin_tube = mw.Tube(length="2 foot", ID="0.04 in", OD="1/16 in", material="PFA")
@@ -74,7 +74,7 @@ def add_rinse():
     global start
     rinse_duration = timedelta(minutes=2)
     P.add([amino_valve, coupling_valve], start=start, duration=rinse_duration, setting="dmf")
-    P.add([amino_pump, coupling_pump], start=start+switching_time, duration=rinse_duration - 2*switching_time, rate="5 mL/min")
+    P.add([amino_pump, coupling_pump], start=start + switching_time, duration=rinse_duration - 2 * switching_time, rate="5 mL/min")
     start += rinse_duration
 
 
@@ -83,18 +83,19 @@ def add_diea_rinse():
     global start
     rinse_duration = timedelta(minutes=2)
     P.add([amino_valve, coupling_valve], start=start, duration=rinse_duration, setting="dmf")
-    P.add([amino_pump, coupling_pump, diea_pump], start=start+switching_time, duration=rinse_duration - 2*switching_time, rate="5 mL/min")
+    P.add([amino_pump, coupling_pump, diea_pump], start=start + switching_time, duration=rinse_duration - 2 * switching_time, rate="5 mL/min")
     start += rinse_duration
+
 
 for base in pna:
     add_rinse()
 
-    # Turn on amino acid, coupling agent, and diea. does not implement about a priming period 
+    # Turn on amino acid, coupling agent, and diea. does not implement about a priming period
     coupling_duration = timedelta(minutes=1, seconds=30)
 
     P.add(amino_valve, start=start, duration=coupling_duration, setting=base)
     P.add(coupling_valve, start=start, duration=coupling_duration, setting="hatu")
-    P.add([amino_pump, coupling_pump, diea_pump], start=start+switching_time, duration=coupling_duration - 2*switching_time, rate="5 mL/min")
+    P.add([amino_pump, coupling_pump, diea_pump], start=start + switching_time, duration=coupling_duration - 2 * switching_time, rate="5 mL/min")
 
     start += coupling_duration
 
@@ -104,7 +105,7 @@ for base in pna:
     # Fmoc removal
     pip_addition_duration = timedelta(minutes=1, seconds=30)
 
-    P.add(amino_pump, start=start+switching_time, duration=pip_addition_duration - 2*switching_time, rate="5 mL/min")
+    P.add(amino_pump, start=start + switching_time, duration=pip_addition_duration - 2 * switching_time, rate="5 mL/min")
     P.add(amino_valve, start=start, duration=pip_addition_duration, setting="dmf")
     P.add(coupling_valve, start=start, duration=pip_addition_duration, setting="dmf")
 
