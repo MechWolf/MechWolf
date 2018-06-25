944093099f0ed5b647c43fd8543bc184af752216
Benjamin Lee <benjamindlee@me.com>
2018-06-25 00:15:41 -0600

    Some getting started info

--- docs/source/guide/getting_started.rst
+++ docs/source/guide/getting_started.rst
@@ -1,11 +1,41 @@
 Getting Started
 ===============
 
-Create an Apparatus
--------------------
+The best way to learn MechWolf is by example. In the spirit of reproducibility,
+we are going to go through the process of setting up and running our peptoid
+synthesizer step-by-step.
 
-Let's start from square one (or zero, if you're counting like a programmer [#f1]_). 
+Create Components
+-----------------
+
+Let's start from square one (or zero, if you're counting like a programmer
+[#f1]_). We first need to import MechWolf:
+
+.. literalinclude:: ../../../examples/peptoid.py
+   :language: python
+   :lines: 1-2
+
+We also imported the :class:`datetime.timedelta` class, for reasons we'll see
+later. The next thing that we need to do is to define the components of our
+synthesizer. It will consist of vessels containing reagents:
+
+.. literalinclude:: ../../../examples/peptoid.py
+   :language: python
+   :lines: 4-8
+
+And the amines:
+
+.. literalinclude:: ../../../examples/peptoid.py
+   :language: python
+   :lines: 10-18
+
+What we have so far are a bunch of :class:`~mechwolf.components.vessel` objects.
+Note that when we instantiate them we give them two arguments. The first,
+required argument is what's in it. The second ``name`` keyword is the name the
+we'll be using to refer to it. Consider the ``name`` to be like a unique
+identifier. In most if not all cases it should match the variable name. However,
+note that the name of the vessel does not need to match what's inside it.
 
 .. rubric:: Footnotes
 
-.. [#f1] If you don't get this joke, go back and read the :doc:`gentle introduction <gentle_intro>` before proceeding.
+.. [#f1] Which you should be. If you don't get this joke, go back and read the :doc:`gentle introduction <gentle_intro>` before proceeding.
--- examples/peptoid.py
+++ examples/peptoid.py
@@ -7,11 +7,6 @@ acid = mw.Vessel("bromoacetic acid", name="acid")
 solvent = mw.Vessel("solvent", name="solvent")
 output = mw.Vessel("waste", name="output")
 
-# define pumps
-coupling_pump = mw.VarianPump(name="pump_3")
-amine_pump = mw.VarianPump(name="pump_2")
-mixer = mw.TMixer()
-
 # define amines
 amine_1 = mw.Vessel("amine_1", name="amine_1")
 amine_2 = mw.Vessel("amine_2", name="amine_2")
@@ -22,8 +17,10 @@ amine_6 = mw.Vessel("amine_6", name="amine_6")
 amine_7 = mw.Vessel("amine_7", name="amine_7")
 amine_8 = mw.Vessel("amine_8", name="amine_8")
 
-# dummy passive heater
-# heater = mw.Component("heater")
+# define pumps
+coupling_pump = mw.VarianPump(name="pump_3")
+amine_pump = mw.VarianPump(name="pump_2")
+mixer = mw.TMixer()
 
 # define valve
 amine_mapping = dict(amine_1=1,
@@ -38,10 +35,7 @@ amine_mapping = dict(amine_1=1,
                      solvent=10)
 valve = mw.ViciValve(name="valve", mapping=amine_mapping)
 
-act_mapping = dict(coupling_agent=1,
-                   solvent=10)
-
-coupling_valve = mw.ViciValve(name="coupling_valve", mapping=act_mapping)
+coupling_valve = mw.ViciValve(name="coupling_valve", mapping=dict(coupling_agent=1, solvent=10))
 
 fat_tube = mw.Tube(length="2 foot", ID="1/16 in", OD="1/8 in", material="PFA")
 thin_tube = mw.Tube(length="2 foot", ID="0.04 in", OD="1/16 in", material="PFA")
@@ -55,8 +49,6 @@ A.add(coupling_pump, mixer, thin_tube)
 A.add([amine_1, amine_2, amine_3, amine_4, amine_5, amine_6, amine_7, amine_8, solvent, acid], valve, fat_tube)
 A.add(valve, amine_pump, fat_tube)
 A.add(amine_pump, mixer, thin_tube)
-# A.add(mixer, heater, thin_tube)
-# A.add(heater, output, thin_tube)
 
 # A.describe()
 A.visualize(graph_attr=dict(splines="ortho", nodesep="0.75"), label_tubes=False)
@@ -99,7 +91,6 @@ for amine in peptoid:
     start += amine_addition_duration
 
     # after amine addition, wash out with just the amine pump
-
     P.add(valve, start=start, duration=amine_addition_duration, setting="solvent")
     P.add(coupling_valve, start=start, duration=amine_addition_duration, setting="solvent")
     P.add(amine_pump, start=start + switching_time, duration=amine_addition_duration - 2 * switching_time, rate="5 mL/min")
