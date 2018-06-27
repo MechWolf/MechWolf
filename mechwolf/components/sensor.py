9873a8ee5e8f6deff3bc725b7a0b4d493993877c
Alex Mijalis <mijalis@gmail.com>
2018-06-26 23:37:38 -0400

    Basic sensor object added 	modified:   mechwolf/components/sensor.py

--- mechwolf/components/sensor.py
+++ mechwolf/components/sensor.py
@@ -1,4 +1,5 @@
 from .component import ActiveComponent
+import asyncio
 
 class Sensor(ActiveComponent):
     """A generic sensor.
@@ -10,12 +11,32 @@ class Sensor(ActiveComponent):
     Attributes:
         name (str, optional): The name of the Sensor.
         active (bool): Whether the sensor is active.
+        interval (int): Data collection interval, in seconds. Default is 1 sec. 
     """
 
-    def __init__(self, name):
+    def __init__(self, name, interval=1):
         super().__init__(name=name)
         self.active = False
+        self.interval = interval
 
     def base_state(self):
         '''Default to being inactive.'''
         return dict(active=False)
+
+    def read_sensor(self):
+        '''Return data to be sent back to the hub.'''
+        #Do data collection task here
+        data = 2
+        return data
+
+    def config(self):
+        '''Default to one second interval'''
+        return {"interval": (int, 1)}
+
+    async def update(self):
+        '''If data collection is off and needs to be turned on, turn it on.
+           If data collection is on and needs to be turned off, turn off and return data.'''
+        while self.active:
+            yield self.read_sensor()
+            await asyncio.sleep(self.interval)
+        
