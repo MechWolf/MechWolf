dd35d460bd67736023eea495230392d78ce04688
Benjamin Lee <benjamindlee@me.com>
2018-11-05 15:59:18 -0500

    Or in init

--- mechwolf/__init__.py
+++ mechwolf/__init__.py
@@ -3,9 +3,6 @@ from pint import UnitRegistry
 # unit registry for conversions
 ureg = UnitRegistry(autoconvert_offset_to_baseunit=True)
 
-RESOLVER_URL = "https://www.mechwolf.io/v1/"
-'''The url of the MechWolf resolver.'''
-
 __version__ = '0.0.1'
 
 # to avoid circular import
