f44f319dcb4803f01643c99196553b9961b4d8e4
tiny <mijalis@gmail.com>
2018-12-14 20:09:17 -0500

    Changes to be committed: 	modified:   varian.py

--- mechwolf/components/varian.py
+++ mechwolf/components/varian.py
@@ -11,10 +11,10 @@ class VarianPump(Pump):
     '''A Varian pump.
     '''
 
-    def __init__(self, name, unit_id='0x80', serial_port=None, max_rate=0):
+    def __init__(self, name, unit_id='0x80', serial_port=None, max_rate="0 mL/min"):
         super().__init__(name=name)
         self.rate = ureg.parse_expression("0 ml/min")
-        self.max_rate = max_rate
+        self.max_rate = ureg.parse_expression(max_rate)
         self.serial_port = serial_port
 
         #VARIAN PUMP SPECIFIC OPTIONS
@@ -32,7 +32,7 @@ class VarianPump(Pump):
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.set_flow(0)
+        self.set_flow(ureg.parse_expression("0 mL/min").to(ureg.ml/ureg.min).magnitude)
         self.ser.close()
 
     def lock(self):
@@ -41,21 +41,24 @@ class VarianPump(Pump):
         print(self.ser.read_all())
 
     def set_flow(self, flow_rate):
-        print(flow_rate)
+
+        max_rate = self.max_rate.to(ureg.ml / ureg.min).magnitude
+        print(f"rate {flow_rate} max_rate {max_rate}")
         #Flow rate must be supplied as an string from 000000 to 100000, where 100000 = 100% of the maximum pump flow rate.
-        percentage = 100000 * flow_rate / self.max_rate
-        print(percentage)
+        percentage = 100000 * flow_rate / max_rate
+        print(f"percentage {percentage}")
         flow_command = [0xFF, self.pump_id, 0x0A, 0x58]
         flow_command.extend(list(str(int(percentage)).zfill(6).encode()))
         flow_command.append(0x0D)
         print(flow_command)
         self.ser.write(flow_command)
-        print(self.ser.read_all())
 
-    async def update(self):
-        new_rate = ureg.parse_expression(self.rate).to(ureg.ml / ureg.min).magnitude
+    def update(self):
+        print(f"old rate {self.rate}")
+        new_rate = self.rate.to(ureg.ml / ureg.min).magnitude
+        print(f"new rate {new_rate}")
         self.set_flow(new_rate)
-        yield { "timestamp": time.time(),
+        return { "timestamp": time.time(),
 		"params": {"rate": str(new_rate)},
 		"device": "self.name"}
 
