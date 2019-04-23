47e74de751bd691cd44cd0696e29e208bd278d2b
Benjamin Lee <benjamindlee@me.com>
2019-04-23 14:17:51 -0400

    Add contributing file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,7 @@ extensions = [
     'sphinxcontrib.programoutput',
     'nbsphinx',
     'sphinx.ext.mathjax',
+    'recommonmark',
 ]
 
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
@@ -58,8 +59,11 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # The master toctree document.
 master_doc = 'index'
