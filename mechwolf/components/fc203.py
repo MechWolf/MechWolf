46f45e3ae84411be2442e2c1d0abe452e6ad0b61
Benjamin Lee <benjamindlee@me.com>
2019-04-23 21:39:17 -0400

    Get rid of annoying u3 error

--- mechwolf/components/fc203.py
+++ mechwolf/components/fc203.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from .component import ActiveComponent
 from .gsioc import GsiocComponent
 
--- mechwolf/components/labjack.py
+++ mechwolf/components/labjack.py
@@ -1,5 +1,4 @@
-import u3
-
+from . import term
 from .sensor import Sensor
 
 
@@ -12,9 +11,13 @@ class LabJack(Sensor):
 
     def __init__(self, name):
         super().__init__(name=name)
+        try:
+            import u3 # noqa
+        except ImportError:
+            raise ImportError(term.red("Unable to create LabJack. No u3 module installed. Try getting it here: https://github.com/labjack/LabJackPython/blob/master/src/u3.py."))
 
     def __enter__(self):
-        self.device = u3.U3()
+        self.device = u3.U3() # noqa
         self.device.configIO(FIOAnalog=15) #Configure FIO 0-3 as analog in
         return self
 
