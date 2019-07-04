594256ef1ce6deb3aa15b5bcb84b6eeeee70591a
Benjamin Lee <benjamindlee@me.com>
2019-07-03 23:43:50 -0400

    Missed a spot

--- mechwolf/__init__.py
+++ mechwolf/__init__.py
@@ -1,11 +1,7 @@
 from pint import UnitRegistry
-from blessings import Terminal
 
 # unit registry for conversions
 ureg = UnitRegistry(autoconvert_offset_to_baseunit=True)
-
-# initialize colored printing
-term = Terminal()
 
 __version__ = "0.0.1"
 
