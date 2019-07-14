520c24b74358e198132228af2353a53100c49369
Benjamin Lee <benjamindlee@me.com>
2019-07-14 17:31:11 -0400

    Custom sidebar for API docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,36 +6,38 @@ module.exports = {
     lastUpdated: "Last Updated", // string | boolean
     repo: "Benjamin-Lee/mechwolf",
     editLinks: true,
-    sidebar: [
-      "/intro",
-      {
-        title: "About",
-        collapsable: false,
-        children: [
-          "/about/faq",
-          "/about/support",
-          "/about/why",
-          "about/license"
-        ]
-      },
-      {
-        title: "Guide",
-        collapsable: false,
-        children: [
-          "/guide/gentle_intro",
-          "/guide/installation",
-          "/guide/getting_started",
-          "/guide/new_components"
-        ]
-      },
-      {
-        title: "API Reference",
-        collapsable: false,
-        children: [
-          "/api/mechwolf",
-          "/api/stdlib_components"
-        ]
-      }
-    ]
+    sidebar: {
+      "/api/": [
+        {
+          title: "API Reference",
+          collapsable: false,
+          children: ["/api/mechwolf", "/api/stdlib_components"]
+        }
+      ],
+      "/": [
+        "/intro",
+        {
+          title: "About",
+          collapsable: false,
+          children: [
+            "/about/faq",
+            "/about/support",
+            "/about/why",
+            "about/license"
+          ]
+        },
+        {
+          title: "Guide",
+          collapsable: false,
+          children: [
+            "/guide/gentle_intro",
+            "/guide/installation",
+            "/guide/getting_started",
+            "/guide/new_components"
+          ]
+        },
+        "/api/"
+      ]
+    }
   }
-};
+}
