3bb28588bdaea275838dac56b95b0345be6aadda
littleblackfish <muzcuk@gmail.com>
2019-07-29 17:29:47 -0400

    ViciPump is now async.

--- mechwolf/components/contrib/vicipump.py
+++ mechwolf/components/contrib/vicipump.py
@@ -23,46 +23,45 @@ class ViciPump(Pump):
         "supported": True,
     }
 
-    def __init__(self, name=None, serial_port=None, volume_per_rev=0):
+    def __init__(self, name, serial_port, volume_per_rev):
         super().__init__(name=name)
         self.rate = ureg.parse_expression("0 ml/min")
         self.serial_port = serial_port
-        self.volume_per_rev = volume_per_rev
+        self.volume_per_rev = ureg.parse_expression(volume_per_rev)
 
     def __enter__(self):
-        import serial
+        import aioserial
 
-        self.ser = serial.Serial(
+        self.ser = aioserial.AioSerial(
             self.serial_port,
             9600,
-            parity=serial.PARITY_NONE,
+            parity=aioserial.PARITY_NONE,
             stopbits=1,
             timeout=0.1,
-            write_timeout=0.1,
-        )
+            write_timeout=0.1 )
 
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.rate = ureg.parse_expression("0 mL/min")
-        self.update()
-        self.ser.close()
+        self.ser.write(b'SL 0\r\n') # Stop pump
+        del self.ser
 
-    def set_flow(self, flow_rate):
+    async def set_flow(self, flow_rate):
 
         steps_per_rev = 51200
         gear_ratio = 9.86
 
-        steps_per_second = steps_per_rev / self.volume_per_rev
+        steps_per_second = steps_per_rev / self.volume_per_rev.to(ureg.ml).magnitude
         steps_per_second *= flow_rate.to(ureg.ml / ureg.sec).magnitude
         steps_per_second *= gear_ratio
         steps_per_second = int(steps_per_second)
 
         flow_command = "SL {}\r\n".format(steps_per_second)
 
-        self.ser.write(flow_command.encode(encoding="ascii"))
+        await self.ser.write_async(flow_command.encode(encoding="ascii"))
 
         self.ser.reset_input_buffer()
 
-    def update(self):
-        self.set_flow(self.rate)
+    async def update(self):
+        await self.set_flow(self.rate)
