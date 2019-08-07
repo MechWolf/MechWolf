63817462ded9fd41bdf4e9bc90a90feda6bc146c
Benjamin Lee <benjamindlee@me.com>
2019-08-07 00:38:07 -0400

    Remove schema folder for now

--- schemas/test_apparatus.sh
+++
@@ -1,4 +0,0 @@
-cat apparatus_example.yaml | yaml2json > apparatus_example.json
-cat mechwolf.schema.yaml | yaml2json >  mechwolf.schema.json
-jsonschema mechwolf.schema.json -i apparatus_example.json
-rm -rf mechwolf.schema.json apparatus_example.json
