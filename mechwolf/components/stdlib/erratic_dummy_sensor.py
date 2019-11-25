cd5ee4daa10e76c15fc7b2c7ebfbca7faa7e5b69
Benjamin Lee <benjamindlee@me.com>
2019-11-25 01:41:45 -0500

    Add more failure modes to erratic dummy sensor

--- mechwolf/components/stdlib/erratic_dummy_sensor.py
+++ mechwolf/components/stdlib/erratic_dummy_sensor.py
@@ -2,6 +2,8 @@ import random
 from math import sin
 from typing import Optional
 
+from loguru import logger
+
 from .sensor import Sensor
 
 
@@ -12,26 +14,70 @@ class ErraticDummySensor(Sensor):
     Don't use this in a real apparatus! It doesn't return real data.
     :::
 
+    Arguments:
+    - `name`: The component's name.
+    - `invocation_threshold`: The minimum number of reads before failure.
+
     Attributes:
     - `name`: The component's name.
     - `rate`: Data collection rate in Hz as a `pint.Quanity`. A rate of 0 Hz corresponds to the sensor being off.
     """
 
-    def __init__(self, name: Optional[str] = None):
+    def __init__(self, name: Optional[str] = None, invocation_threshold=100):
         super().__init__(name=name)
         self._unit = "Dimensionless"
-        self._counter = 0.0
-        self._fail_silently = False
+        self._fail = ""
+        self._invocations = 0
+        self._invocation_threshold = invocation_threshold
+        self._value = 0.0
         self._last_value = 0.0
+        self._inc = 0.1
+        self._amp = 2.0
 
     async def _read(self) -> float:
         """Collect the data."""
-        if self._fail_silently:
-            return 0
-        self._fail_silently = (
-            random.choices([True, False], weights=[1, 99])[0] and self._counter > 100
-        )
-        self._counter += 0.2
-        return_value = sin(self._counter) + (((random.random() * 2) - 1) / 20) + 1
-        self._last_value = return_value
+
+        if self._invocations > self._invocation_threshold and not self._fail:
+            self._fail = random.choices(
+                [
+                    "",
+                    "speed up",
+                    "slow down",
+                    "output same",
+                    "output zero",
+                    "change amp",
+                ],
+                weights=[99, 0.2, 0.2, 0.2, 0.2, 0.2],
+            )[0]
+
+            if self._fail == "speed up":
+                self._inc = 0.2
+                logger.warning(f"{self} has encountered an anomaly and sped up!")
+            elif self._fail == "slow down":
+                self._inc = 0.05
+                logger.warning(f"{self} has encountered an anomaly and slowed down!")
+            elif self._fail == "output same":
+                logger.warning(
+                    f"{self} has encountered an anomaly and started outputting the same data"
+                )
+            elif self._fail == "output zero":
+                logger.warning(
+                    f"{self} has encountered an anomaly and started outputting zero"
+                )
+            elif self._fail == "change amp":
+                self._amp = random.choice(list(range(2, 10)))
+                logger.warning(
+                    f"{self} has encountered an anomaly and changed amplitude"
+                )
+
+        if self._fail == "output same":
+            return_value = self._last_value
+        elif self._fail == "output zero":
+            return_value = 0
+        else:
+            self._value += self._inc
+            return_value = ((sin(self._value) + 1) / self._amp) + (random.random() / 20)
+            self._last_value = return_value
+
+        self._invocations += 1
         return return_value
